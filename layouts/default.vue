d79d8cf08a898f6df3dc036489281d3a73ad5b49
TOP TECH 777 <dev.opensoft@gmail.com>
2020-12-18 16:23:18 +0700

    Initial Project

---
+++ layouts/default.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/parent">Parent</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <img src="https://rhtm4.sse.codesandbox.io/_nuxt/assets/logo.svg" />
+      <Nuxt />
+    </main>
+  </div>
+</template>
+
+<style>
+:root {
+  --primary-color: #00c58e;
+}
+
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  margin: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+nav a:hover {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+main {
+  margin: 0 auto;
+  margin-top: 100px;
+  padding: 0 1rem;
+  max-width: 1280px;
+  text-align: center;
+}
+
+img {
+  margin-bottom: 1rem;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+li {
+  margin: 0 0.5rem;
+  padding: 0.25rem;
+  font-size: 1.2rem;
+}
+
+nav {
+  padding: 0 1rem;
+}
+a.nuxt-link-exact-active {
+  color: #00c58e;
+}
+</style>
